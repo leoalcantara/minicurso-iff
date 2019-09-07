04e363fb4160cfa924b07b2eb78bffc5205dc7bc
leoalcantara <leonidio.ti@gmail.com>
2019-09-07 20:05:03 -0300

    Criando o componente contador

---
+++ src/components/Contador.js
@@ -0,0 +1,20 @@
+﻿import React from 'react';
+
+class Contador extends React.Component{    
+    constructor(props){
+        super(props);
+        this.state = {
+            contador: props.valorInicial
+        }
+    }
+    render(){
+       return (
+        <div>   
+            <h1>Você clicou:{this.state.contador} </h1>
+            <button onClick={() => this.setState({contador: this.state.contador + 1})}>+</button> 
+           
+        </div>
+       )
+    }
+}
+export default Contador;
